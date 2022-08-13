5140ecbb9a140389774efc203a53292893bd7404
Igor <garkol@bk.ru>
2022-08-13 15:05:59 +0300

    task additional

---
+++ HomeWork07_add/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задана целочисленная матрица, состоящая из N строк и M столбцов. 
+//Требуется транспонировать ее относительно горизонтали
+
+Console.Write("Задайте количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+        Console.Write($"{array[i, j]}" + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+int[,] trans = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        trans[i, j] = array[m - i - 1, j];
+        Console.Write($"{trans[i, j]}" + "\t");
+    }
+Console.WriteLine();
+}
