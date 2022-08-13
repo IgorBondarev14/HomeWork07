88110a277e02be25159e5c7a283705683fa89566
Igor <garkol@bk.ru>
2022-08-13 13:47:40 +0300

    task three

---
+++ HomeWork07_03/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое 
+// элементов в каждом столбце.
+
+Console.Write("Задайте количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+        Console.Write($"{array[i, j]}" + "\t");
+    }
+    Console.WriteLine();
+}
+double[] arithmetic = new double[n];
+for (int i = 0; i < n; i++)
+{
+    arithmetic[i] = 0;
+    for (int j = 0; j < m; j++)
+        arithmetic[i] = (arithmetic[i] + array[j, i]);
+    arithmetic[i] = arithmetic[i] / n;
+}
+
+Console.Write("[" + string.Join(";   ", arithmetic) + "]");
+Console.WriteLine();
